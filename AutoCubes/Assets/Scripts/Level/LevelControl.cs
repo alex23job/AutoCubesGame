9b655eb13543e94e260283177c8804558bc84079
alex23job <106354657+alex23job@users.noreply.github.com>
2025-09-21 16:52:38 +0300

    Adding 4 spawn cars point

--- AutoCubes/Assets/Scripts/Level/LevelControl.cs
+++ AutoCubes/Assets/Scripts/Level/LevelControl.cs
@@ -1,14 +1,21 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelControl : MonoBehaviour
 {
     [SerializeField] private SpawnCars[] spawnCars;
 
+    private List<GameObject> cars = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SpawnCar(0);
         SpawnCar(1);
+        SpawnCar(2);
+        SpawnCar(3);
+        Invoke("CarsToWay", 10f);
     }
 
     // Update is called once per frame
@@ -19,6 +26,19 @@ public class LevelControl : MonoBehaviour
 
     private void SpawnCar(int numSpawnPoint)
     {
-        spawnCars[numSpawnPoint].SpawnCar();
+        GameObject car = spawnCars[numSpawnPoint].SpawnCar();
+        if (car != null) cars.Add(car); 
+    }
+
+    private void CarsToWay()
+    {
+        foreach (GameObject car in cars)
+        {
+            CarControl carControl = car.GetComponent<CarControl>();
+            if (carControl != null)
+            {
+                carControl.CarToWay();
+            }
+        }
     }
 }

494112863d0069629362c5bcf9570e6863e7725b
alex23job <106354657+alex23job@users.noreply.github.com>
2025-10-31 16:16:35 +0300

    Adding prev button to avtoshow

--- AutoCubes/Assets/Scripts/Garage/AutoShowControl.cs
+++ AutoCubes/Assets/Scripts/Garage/AutoShowControl.cs
@@ -25,16 +25,28 @@ public class AutoShowControl : MonoBehaviour
         NextCar();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void BoardRotate()
     {
         isRotate = !isRotate;
         anim.SetBool("IsRotate", isRotate);
+    }
+
+    public void PrevCar()
+    {
+        if (currentViewCar != -1) transform.GetChild(currentViewCar + 1).gameObject.SetActive(false);
+        currentViewCar += 6;
+        currentViewCar %= 7;
+        transform.GetChild(currentViewCar + 1).gameObject.SetActive(true);
+        CarPassport carPassport = transform.GetChild(currentViewCar + 1).gameObject.GetComponent<CarPassport>();
+        if (carPassport != null)
+        {
+            if ((carPassport.ExpForSale <= GameManager.Instance.currentPlayer.totalScore) && (carPassport.PriceCar <= GameManager.Instance.currentPlayer.totalGold))
+            {
+                btnBuy.interactable = true;
+            }
+            else { btnBuy.interactable = false; }
+            ViewParams(carPassport);
+        }
     }
 
     public void NextCar()
